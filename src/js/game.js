251411ccb40b3a1a3420399cd0af8b0851c868ed
Sergey Falov <srg.falov@yandex.ru>
2024-09-30 08:38:00 +0300

    Первый коммит

---
+++ src/js/game.js
@@ -0,0 +1,18 @@
+import './domain';
+export default class Game {
+    start() {
+        console.log('game started');
+    }
+}
+
+export class GameSavingData {
+
+}
+
+export function readGameSaving() {
+
+}
+
+export function writeGameSaving() {
+
+}
